6bdc6db28d604dab1e5aef2269bbc301a299ecbe
Michell Lopez <ingmichell5101@gmail.com>
2024-05-26 22:27:34 -0600

    Se agrega la ruta hacia la tienda

--- distribuidoraHGO/urls.py
+++ distribuidoraHGO/urls.py
@@ -20,4 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",include("website.urls",namespace="website")),
+    path("store/",include("store.urls",namespace="store")),
 ]
